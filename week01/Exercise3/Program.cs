8fb06f2617aac523086efbaff4dd85573994ea5a
Secret-Doughnut <Dustinorangecook@gmail.com>
2025-09-04 16:29:11 -0600

    Finished the Magic Number assignment.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string check = "";
+
+        Random RandomNumber = new Random();
+        int Number = RandomNumber.Next(1, 101);
+
+            while (check == "")
+        {
+            Console.Write("What is your guess? ");
+            string MagicGuess = Console.ReadLine();
+            int Guess = int.Parse(MagicGuess);
+
+            if (Number > Guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (Guess > Number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (Number == Guess)
+            {
+                Console.Write("You guessed it!");
+                check = "yes";
+            }
+            else
+            {
+                Console.Write("Error. Please try again.");
+                check = "yes";
+            }
+        }
     }
 }
