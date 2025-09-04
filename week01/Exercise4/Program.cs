0dee8d02cc422e6ac394e8135e2b90d8b615a778
Secret-Doughnut <Dustinorangecook@gmail.com>
2025-09-04 17:24:12 -0600

    Finished the Number List Inator... Thingie.

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,49 @@
-using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> Numbers = new List<int>();
+        string Check = "";
+        int sum = 0;
+        int BiggestNumber = 0;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        while (Check != "yes")
+        {
+            Console.Write("Enter number: ");
+            string Catcher = Console.ReadLine();
+            int ListNumber = int.Parse(Catcher);
+
+            if (ListNumber != 0)
+            {
+                Numbers.Add(ListNumber);
+            }
+            else if (ListNumber == 0)
+            {
+                Check = "yes";
+            }
+            else
+            {
+                Console.Write("Something went wrong)");
+            }
+        }
+
+        foreach (int Number in Numbers)
+        {
+            sum += Number;
+            if (Number > BiggestNumber)
+            {
+                BiggestNumber = Number;
+            }
+        }
+        double AverageAnswer = Numbers.Count;
+        double Average = sum / AverageAnswer;
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {Average}");
+        Console.WriteLine($"The largest number is: {BiggestNumber}");
     }
 }
