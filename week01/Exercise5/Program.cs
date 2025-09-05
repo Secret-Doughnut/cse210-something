f2e7f40369e46f57bb30b82f03676811febdf900
Secret-Doughnut <Dustinorangecook@gmail.com>
2025-09-04 18:54:05 -0600

    Finished the function assignment.

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string Name = Console.ReadLine();
+            return Name;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            string Catcher = Console.ReadLine();
+            int Number = int.Parse(Catcher);
+            return Number;
+        }
+
+        static double SquareNumber(int Number)
+        {
+            double Answer = Number * Number;
+            return Answer;
+        }
+
+        static void DisplayResults(string Name, double Answer)
+        {
+            Console.WriteLine($"{Name}, the square of your number is {Answer}");
+        }
+
+        DisplayWelcome();
+        string Name = PromptUserName();
+        int Number = PromptUserNumber();
+        double Answer = SquareNumber(Number);
+        DisplayResults(Name, Answer);
+
     }
 }
