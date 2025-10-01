a0f939c822b8c2afebf3c868ec56bf4e82b423fd
Secret-Doughnut <Dustinorangecook@gmail.com>
2025-10-01 13:36:04 -0600

    Finished my Homework assignment.

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment1 = new Assignment("Maron", "Speling");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment assignment2 = new MathAssignment("Jake", "Fractions", "Section 3", "Problems: 30-72");
+        Console.WriteLine(assignment2.GetHomeworkList());
+        Console.WriteLine(assignment2.GetSummary());
+
+
+        WritingAssignment assignment3 = new WritingAssignment("Maron", "spelling", "How proper speling can avoid causing problems");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
     }
 }
